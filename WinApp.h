0fb536bb428c04b3c090e20ef5bac859998262bb
Oguro Haruka <Oguro Haruka@MSI>
2023-10-24 15:19:46 +0900

    03_03

--- WinApp.h
+++ WinApp.h
@@ -11,5 +11,15 @@ public:
 
 	void Update();
 
+	HWND GetHwnd() const { return hwnd; }
+	HINSTANCE GetHInstance()const { return w.hInstance; }
+public:
+	// ウィンドウサイズ
+	static const int window_width = 1280;  // 横幅
+	static const int window_height = 720;  // 縦幅
+
+private:
+	HWND hwnd;
+	WNDCLASSEX w{};
 };
 
